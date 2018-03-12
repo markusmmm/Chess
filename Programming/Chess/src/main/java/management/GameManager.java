e0d9d1325084377adb4e4f8e42a4cc9c8efb058d
Mikael Røsbak Hanssen <nuc006@uib.no>
2018-03-12 20:55:37 +0100

    Add placeholder functionality for GameManager (to be used until GUI has been implemented)

--- Programming/Chess/src/main/java/management/GameManager.java
+++ Programming/Chess/src/main/java/management/GameManager.java
@@ -1,20 +1,44 @@
 package management;
 
+import resources.Vector2;
+
+import java.util.Scanner;
+
 public class GameManager {
 
 	private static Board board = null;
 
-	public static void main() {
-		// TODO - implement GameManager.main
-		throw new UnsupportedOperationException();
+	public static void main(String[] args) {
+	    board = new Board(2,8, false);
+        int playerI = 0;
+
+        while(true) {
+            if(step(playerI))
+                playerI++;
+        }
 	}
 
 	/**
-	 * A looping function that steps through the game at a fixed time interval
+	 * Takes in input from the active player, and attempts to perform the given move
+     * NOTE: The code below is a placeholder functionality, until UI has been implemented
 	 */
-	public static void step() {
-		// TODO - implement GameManager.step
-		throw new UnsupportedOperationException();
+	public static boolean step(int playerI) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Player " + (playerI + 1) + "'s turn");
+		System.out.println("Enter start pos: ");
+		int x0 = in.nextInt(), y0 = in.nextInt();
+        System.out.println("Enter end pos: ");
+        int x1 = in.nextInt(), y1 = in.nextInt();
+        in.close();
+
+        Vector2 start = new Vector2(x0, y0),
+                end = new Vector2(x1, y1);
+
+        if(board.movePiece(playerI, start, end))
+            return true;
+
+        System.out.println("Invalid move!");
+        return false;
 	}
 
 }
