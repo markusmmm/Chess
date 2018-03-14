34688255a6af995c2d73516e7bdd9ab0b54b3653
Markus Håvåg Mæhlum <markus.mehlum@student.uib.no>
2018-03-14 21:06:23 +0100

    bishop

--- Programming/Chess/src/main/java/pieces/Bishop.java
+++ Programming/Chess/src/main/java/pieces/Bishop.java
@@ -1,13 +1,44 @@
 package pieces;
 
-import resources.*;
+
 import management.*;
+import resources.Piece;
+import resources.Vector2;
+
 
 public class Bishop extends ChessPiece {
 
+	private final boolean canJump;
+	private final Piece piece;
+	private Vector2 position;
+
+	public Bishop(Vector2 position){
+		super();
+		canJump = false;
+		piece = Piece.BISHOP;
+		this.position = position;
+
+	}
+
 	public Piece piece() {
-		// TODO - implement Bishop.piece
-		throw new UnsupportedOperationException();
+		return piece;
+	}
+
+	/**
+	 * checks one by one position from this position
+	 * toward destination, returns false if runs into another piece
+	 */
+	private boolean freePath(Vector2 destination, Board board) {
+		Vector2 path = null;
+		int between = this.position.distance(destination) - 1;
+
+		for (int step = 0; step < between; step++) {
+			path = path.stepToward(destination);
+			if (board.getPiece(path) != null) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
@@ -16,22 +47,26 @@ public class Bishop extends ChessPiece {
 	 * @param board
 	 */
 	public boolean legalMove(Vector2 move, Board board) {
-		// TODO - implement Bishop.legalMove
-		throw new UnsupportedOperationException();
+		return (
+				positiveCoordinates(move) &&
+						inDiagonals(move) &&
+						freePath(move, board)
+		);
+	}
+
+	private boolean inDiagonals(Vector2 newPos) {
+		return Math.abs(this.position.getX() - newPos.getX()) == Math.abs(this.position.getY() - newPos.getY());
 	}
 
 	public boolean canJump() {
-		// TODO - implement Bishop.canJump
-		throw new UnsupportedOperationException();
+		return canJump;
+
 	}
 
-	/**
-	 * 
-	 * @param position
-	 */
-	public Bishop(Vector2 position) {
-		// TODO - implement Bishop.Bishop
-		throw new UnsupportedOperationException();
+	private boolean positiveCoordinates(Vector2 pos) {
+		return 0 <= pos.getX() && 0 <= pos.getY();
 	}
+
+
 
 }
