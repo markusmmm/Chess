d055decb0e460c73b53ab574b3f78abd351486de
Markus Håvåg Mæhlum <markus.mehlum@student.uib.no>
2018-03-14 21:39:55 +0100

    made get possibleMoves in bishop

--- Programming/Chess/src/main/java/pieces/Bishop.java
+++ Programming/Chess/src/main/java/pieces/Bishop.java
@@ -5,12 +5,16 @@ import management.*;
 import resources.Piece;
 import resources.Vector2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Bishop extends ChessPiece {
 
 	private final boolean canJump;
 	private final Piece piece;
 	private Vector2 position;
+
 
 	public Bishop(Vector2 position){
 		super();
@@ -19,6 +23,66 @@ public class Bishop extends ChessPiece {
 		this.position = position;
 
 	}
+
+	/**
+	 *
+	 * @param board
+	 * @return a list of all possible moves from this position
+	 */
+
+	// TODO: board should return its size. board.getSize/getDimentions
+
+	public List<Vector2> getPossibleMoves(Board board){
+
+		int BoardSize = 8;
+		List<Vector2> possibleMoves = new ArrayList<>();
+
+		int row = position.getX();
+		int collumn = position.getY();
+
+
+		for(int j = collumn + 1, i = row + 1; j < 8 && i < 8; j++, i++){
+			Vector2 move = new Vector2(i,j);
+			if(legalMove(move,board))
+					possibleMoves.add(new Vector2(i, j));
+			else
+				break; // TODO: Should also check if it hits an enemy.
+
+			}
+
+
+
+		for(int j = collumn - 1, i = row + 1; j < -1 && i < 8; j--, i++){
+			Vector2 move = new Vector2(i,j);
+			if(legalMove(move,board))
+				possibleMoves.add(new Vector2(i, j));
+			else
+				break; // TODO: Should also check if it hits an enemy.
+
+		}
+
+		for(int j = collumn - 1, i = row - 1; j < -1 && i < -1; j--, i--){
+			Vector2 move = new Vector2(i,j);
+			if(legalMove(move,board))
+				possibleMoves.add(new Vector2(i, j));
+			else
+				break; // TODO: Should also check if it hits an enemy.
+
+		}
+
+		for(int j = collumn + 1, i = row - 1; j < 8 && i < -1; j++, i--){
+			Vector2 move = new Vector2(i,j);
+			if(legalMove(move,board))
+				possibleMoves.add(new Vector2(i, j));
+			else
+				break; // TODO: Should also check if it hits an enemy.
+
+		}
+
+		return possibleMoves;
+	}
+
+
 
 	public Piece piece() {
 		return piece;
