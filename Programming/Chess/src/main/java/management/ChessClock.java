53dc3fc460255b4f183e6f2801b143021b206662
nuc006 <nuc006@student.uib.no>
2018-03-15 15:44:22 +0100

    [Programming]: Add implementation of ChessClock

--- Programming/Chess/src/main/java/management/ChessClock.java
+++ Programming/Chess/src/main/java/management/ChessClock.java
@@ -1,52 +1,67 @@
 package management;
 
 public class ChessClock {
+	private final long GIGA = 1000000000;
 
-	private int bonusTime;
-	private int bonusTimeLimit;
-
-	public void start() {
-		// TODO - implement ChessClock.start
-		throw new UnsupportedOperationException();
-	}
-
-	public void stop() {
-		// TODO - implement ChessClock.stop
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean endTurn() {
-		// TODO - implement ChessClock.endTurn
-		throw new UnsupportedOperationException();
-	}
+	private int playerI = 0;
+	private int nPlayers;
+	private long[] durations;
+	private long bonusTime, bonusTimeLimit;
+	private long startTime;
+	private long moveStartTime;
 
 	/**
-	 * 
-	 * @param playerIndex
-	 */
-	public void timeLeft(int playerIndex) {
-		// TODO - implement ChessClock.timeLeft
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param playerIndex
-	 */
-	public void setPlayer(int playerIndex) {
-		// TODO - implement ChessClock.setPlayer
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
+	 *
 	 * @param nPlayers
 	 * @param bonus
 	 * @param bonusLimit
 	 */
-	public ChessClock(int nPlayers, double duration, double bonus, double bonusLimit) {
-		// TODO - implement ChessClock.ChessClock
-		throw new UnsupportedOperationException();
+	public ChessClock(int nPlayers, int duration, double bonus, double bonusLimit) {
+		this.nPlayers = nPlayers;
+		durations = new long[nPlayers];
+		for(int i = 0; i < nPlayers; i++) {
+			durations[i] = toNanoTime(duration);
+		}
+
+		bonusTime = toNanoTime(bonus);
+		bonusTimeLimit = toNanoTime(bonusLimit);
+
+		reset();
 	}
 
+	public void reset() {
+		startTime = System.nanoTime();
+		moveStartTime = startTime;
+	}
+
+	public boolean endTurn() {
+		long delta = System.nanoTime() - moveStartTime;
+		if(delta <= 0) return false;
+
+		if(bonusTimeLimit < 0 || delta <= bonusTimeLimit) {
+			durations[playerI] += bonusTime;
+		}
+
+		if(++playerI >= nPlayers)
+			playerI = 0;
+
+		moveStartTime = System.nanoTime();
+		return true;
+	}
+
+	/**
+	 * 
+	 * @param playerIndex
+	 */
+	public double timeLeft(int playerIndex) {
+		long nanoTimeLeft = startTime + durations[playerIndex] - System.nanoTime();
+		return fromNanoTime(nanoTimeLeft);
+	}
+
+	private long toNanoTime(double time) {
+		return Math.round(time) * GIGA;
+	}
+	private double fromNanoTime(long nanoTime) {
+		return nanoTime / (double)GIGA;
+	}
 }
