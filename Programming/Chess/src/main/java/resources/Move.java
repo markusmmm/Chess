60f4ffe6861660bfc4f546e2df2dfe0e4654f4ce
nuc006 <nuc006@student.uib.no>
2018-03-20 10:32:33 +0100

    Add tuple-class, defining a move

---
+++ Programming/Chess/src/main/java/resources/Move.java
@@ -0,0 +1,10 @@
+package resources;
+
+public class Move {
+    public final Vector2 start, end;
+
+    public Move(Vector2 start, Vector2 end) {
+        this.start = start;
+        this.end = end;
+    }
+}
