e2d948400eb8161631391647e5e23046e577db18
jbe091 <jbe091@uib.no>
2018-03-20 14:01:49 +0100

    shortened queen get allpossible moves

--- Programming/Chess/src/main/java/pieces/Queen.java
+++ Programming/Chess/src/main/java/pieces/Queen.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class Queen extends ChessPiece {
 
     private ArrayList<Vector2> possibleMoves = new ArrayList(Vector2);
+    private ArrayList<Vector2> legalMoves = new ArrayList(Vector2);
 
     public Queen(Vector2 position, Alliance alliance, Board board){
 		super(position, alliance, board, false, Piece.QUEEN);
@@ -24,7 +25,7 @@ public class Queen extends ChessPiece {
 	 */
 	public boolean legalMove(Vector2 move) {
 		return (
-		        positiveCoordinates(move) &&
+		        insideBoard(move) &&
                         inDiagonals(move) &&
                         inStraights(move) &&
                         freePath(move)
@@ -36,29 +37,26 @@ public class Queen extends ChessPiece {
 	    Vector2 mem;
         for (int variable = 0; variable < board.getSize(); variable++) {
             //Straights
-            mem = new Vector2(position.getX(), position.getY() + variable);
-            if(legalMove(mem)) possibleMoves.add(mem);
-            mem = new Vector2(position.getX(), position.getY() - variable);
-            if(legalMove(mem)) possibleMoves.add(mem);
-            mem = new Vector2(position.getX() + variable, position.getY());
-            if(legalMove(mem)) possibleMoves.add(mem);
-            mem = new Vector2(position.getX() - variable, position.getY());
-            if(legalMove(mem)) possibleMoves.add(mem);
+            possibleMoves.add(new Vector2(position.getX(), position.getY() + variable));
+            possibleMoves.add(new Vector2(position.getX(), position.getY() - variable));
+            possibleMoves.add(new Vector2(position.getX() + variable, position.getY()));
+            possibleMoves.add(new Vector2(position.getX() - variable, position.getY()));
 
             //diagonals
-            mem = new Vector2(position.getX() + variable, position.getY() + variable);
-            if(legalMove(mem)) possibleMoves.add(mem);
-            mem = new Vector2(position.getX() + variable, position.getY() - variable);
-            if(legalMove(mem)) possibleMoves.add(mem);
-            mem = new Vector2(position.getX() - variable, position.getY() + variable);
-            if(legalMove(mem)) possibleMoves.add(mem);
-            mem = new Vector2(position.getX() - variable, position.getY() - variable);
-            if(legalMove(mem)) possibleMoves.add(mem);
+            possibleMoves.add(new Vector2(position.getX() + variable, position.getY() + variable));
+            possibleMoves.add(new Vector2(position.getX() + variable, position.getY() - variable));
+            possibleMoves.add(new Vector2(position.getX() - variable, position.getY() + variable));
+            possibleMoves.add(new Vector2(position.getX() - variable, position.getY() - variable));
         }
-        return possibleMoves;
+        for(Vector2 vector: possibleMoves) {
+            if(insideBoard(vector) && freePath(vector)) legalMoves.add(vector);
+        }
+        return legalMoves;
     }
 
-	private boolean positiveCoordinates(Vector2 pos) {
-	    return 0 <= pos.getX() && 0 <= pos.getY();
+    private boolean insideBoard(Vector2 vector) {
+        return (
+                0 <= vector.getX() && vector.getX() < board.getSize() &&
+                0 <= vector.getY() && vector.getY() < board.getSize());
     }
 }
