98f110ef91b522445b8de61f03a41c67184eb531
jbe091 <jbe091@uib.no>
2018-03-20 14:06:45 +0100

    correction queen

--- Programming/Chess/src/main/java/pieces/Queen.java
+++ Programming/Chess/src/main/java/pieces/Queen.java
@@ -34,7 +34,7 @@ public class Queen extends ChessPiece {
 
 	public List<Vector2> getPossibleMoves() {
 	    possibleMoves.clear();
-	    Vector2 mem;
+	    legalMoves.clear();
         for (int variable = 0; variable < board.getSize(); variable++) {
             //Straights
             possibleMoves.add(new Vector2(position.getX(), position.getY() + variable));
