12c7af8a1c1128f33e72aa1e9e04683db8262d7a
jbe091 <jbe091@uib.no>
2018-03-21 19:34:08 +0100

    modified bishop with queens possible moves

--- Programming/Chess/src/main/java/pieces/Bishop.java
+++ Programming/Chess/src/main/java/pieces/Bishop.java
@@ -13,7 +13,7 @@ import java.util.Set;
 
 
 public class Bishop extends ChessPiece {
-
+	private Set<Vector2> possibleMoves = new HashSet<>();
 	public Bishop(Vector2 position, Alliance alliance, Board board){
 		super(position, alliance, board, false, Piece.BISHOP);
 	}
@@ -27,64 +27,19 @@ public class Bishop extends ChessPiece {
 	 */
 
 
-	public Set<Vector2> getPossibleDestinations(){
-
-		Set<Vector2> possibleMoves = new HashSet<Vector2>();
-
-		int row = position.getX();
-		int column = position.getY();
-
-
-		for(int j = column + 1, i = row + 1; j < board.getSize() && i < 8; j++, i++){
-			Vector2 move = new Vector2(i,j);
-			if(legalMove(move))
-					possibleMoves.add(new Vector2(i, j));
-			if(board.getPiece(move) != null){
-				if(board.getPiece(move).alliance() != this.alliance) possibleMoves.add(move);
-			}
-			else break;
-
-			}
-
-
-
-		for(int j = column - 1, i = row + 1; j > -1 && i < 8; j--, i++){
-			Vector2 move = new Vector2(i,j);
-			if(legalMove(move))
-				possibleMoves.add(new Vector2(i, j));
-			if(board.getPiece(move) != null){
-				if(board.getPiece(move).alliance() != this.alliance) possibleMoves.add(move);
-			}
-			else
-				break;
-
+	public Set<Vector2> getPossibleDestinations() {
+		possibleMoves.clear();
+		for (int variable = 0; variable < board.getSize(); variable++) {
+			//diagonals
+			evalMove(new Vector2(position.getX() + variable, position.getY() + variable));
+			evalMove(new Vector2(position.getX() + variable, position.getY() - variable));
+			evalMove(new Vector2(position.getX() - variable, position.getY() + variable));
+			evalMove(new Vector2(position.getX() - variable, position.getY() - variable));
 		}
-
-		for(int j = column - 1, i = row - 1; j > -1 && i > -1; j--, i--){
-			Vector2 move = new Vector2(i,j);
-			if(legalMove(move))
-				possibleMoves.add(new Vector2(i, j));
-			if(board.getPiece(move) != null){
-				if(board.getPiece(move).alliance() != this.alliance) possibleMoves.add(move);
-			}
-			else
-				break;
-
-		}
-
-		for(int j = column + 1, i = row - 1; j < board.getSize() && i > -1; j++, i--){
-			Vector2 move = new Vector2(i,j);
-			if(legalMove(move))
-				possibleMoves.add(new Vector2(i, j));
-			if(board.getPiece(move) != null){
-				if(board.getPiece(move).alliance() != this.alliance) possibleMoves.add(move);
-			}
-			else
-				break;
-
-		}
-
 		return possibleMoves;
+	}
+	private void evalMove(Vector2 vector) {
+		if(legalMove(vector)) possibleMoves.add(vector);
 	}
 
 	/**
