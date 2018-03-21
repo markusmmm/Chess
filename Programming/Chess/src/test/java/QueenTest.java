6bc9fb2f60833663ac0acbdb68b7e3da4a4b66bd
jbe091 <jbe091@uib.no>
2018-03-21 18:23:42 +0100

    test for queen legalMove straight and diagonal added

--- Programming/Chess/src/test/java/QueenTest.java
+++ Programming/Chess/src/test/java/QueenTest.java
@@ -4,13 +4,26 @@ import pieces.Queen;
 import resources.Alliance;
 import resources.Vector2;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class QueenTest {
+
     @Test
-    public void queenMooveDiagonalTest() {
+    public void queenMoveDiagonalTest() {
         Queen queen = new Queen(new Vector2(3,3), Alliance.BLACK, new Board(8,false));
-        queen.legalMove(new Vector2(7,7));
-        queen.legalMove(new Vector2(7,0));
-        queen.legalMove(new Vector2(0,7));
-        queen.legalMove(new Vector2(0,0));
+
+        assertTrue(queen.legalMove(new Vector2(5,5)));
+        assertTrue(queen.legalMove(new Vector2(2,2)));
+        assertTrue(queen.legalMove(new Vector2(2,4)));
+        assertTrue(queen.legalMove(new Vector2(1,5)));
+    }
+
+    @Test
+    public void queenMoveStraightTest() {
+        Queen queen = new Queen(new Vector2(3,3), Alliance.BLACK, new Board(8,false));
+        assertTrue(queen.legalMove(new Vector2(0,3)));
+        assertTrue(queen.legalMove(new Vector2(3,2)));
+        assertTrue(queen.legalMove(new Vector2(7,3)));
+        assertTrue(queen.legalMove(new Vector2(3,5)));
     }
 }
