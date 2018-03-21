360279f543aef6d1542595a9b74719edd6dddfa4
Viet Hoang Nguyen <viet.h.nguyen@student.uib.no>
2018-03-21 17:29:47 +0100

    Tests for pawn

--- Programming/Chess/src/test/java/PawnTest.java
+++ Programming/Chess/src/test/java/PawnTest.java
@@ -1,29 +1,38 @@
 import management.Board;
 import org.junit.jupiter.api.Test;
 import pieces.Pawn;
+import resources.Alliance;
+import resources.Piece;
 import resources.Vector2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class PawnTest {
+public class PawnTest
+{
     @Test
-    public void PawnMoveForwardTest() {
-        Board board = new Board(8, false);
-        Vector2 piecePos = new Vector2(0,1);
-        Vector2 move = new Vector2(0, 2);
-        board.movePiece(piecePos, move);
-        assertEquals(board.getPiece(move).position(), move);
-    }
-    @Test
-    public void PawnMoveToFarTest() {
-        Board board = new Board(8, false);
-        Vector2 piecePos = new Vector2(0,1);
-        Vector2 move = new Vector2(0, 3);
-        board.movePiece(piecePos, move);
-        if (board.getPiece(move) instanceof Pawn){
-            fail("");
-        }
+    public void movePawnForward()
+    {
+
+        Piece[] testBoard = new Piece[]
+                {
+                Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY,
+                Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY,
+                Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY,
+                Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY,
+                Piece.PAWN, Piece.PAWN, Piece.PAWN, Piece.PAWN, Piece.PAWN, Piece.PAWN, Piece.PAWN, Piece.PAWN
+                };
+        Board board = new Board(8, false, testBoard);
+        Vector2 pawnIWantToMove = new Vector2(0,4);
+        Pawn testWhitePawn = (Pawn) board.getPiece(pawnIWantToMove);
+        Vector2 amountOfYTilesIWantToMove = new Vector2(0, -1);
+
+        System.out.println("Position before moving" + testWhitePawn.position());
+        board.movePiece(testWhitePawn.position(), testWhitePawn.position().add(amountOfYTilesIWantToMove));
+        System.out.println("Position after moving" + testWhitePawn.position());
+        System.out.println();
+
+        assert(testWhitePawn != null);
     }
 }
