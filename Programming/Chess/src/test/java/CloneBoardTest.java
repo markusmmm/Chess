e0ff16e21d4c2ac0ed171aff5c2760c45f6292eb
jbe091 <jbe091@uib.no>
2018-03-23 13:17:05 +0100

    added tests for cloning the board

---
+++ Programming/Chess/src/test/java/CloneBoardTest.java
@@ -0,0 +1,31 @@
+import management.Board;
+import org.junit.jupiter.api.Test;
+import resources.Move;
+import resources.Piece;
+import resources.Vector2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CloneBoardTest {
+    @Test
+    public void CloneCantChangeOrgTest() {
+        Board org = new Board(8,false);
+        Board clone = org.clone();
+        Vector2 start = new Vector2(0,1);
+        Vector2 end = new Vector2(0,2);
+        clone.movePiece(new Move(start, end));
+        assertTrue(org.vacant(end));
+        assertTrue(!org.vacant(start));
+    }
+    @Test
+    public void CloneOrgAgainResets() {
+        Board org = new Board(8,false);
+        Board clone = org.clone();
+        Vector2 start = new Vector2(0,1);
+        Vector2 end = new Vector2(0,2);
+        clone.movePiece(new Move(start, end));
+        clone = org.clone();
+        assertTrue(clone.vacant(end));
+        assertTrue(!clone.vacant(start));
+    }
+}
