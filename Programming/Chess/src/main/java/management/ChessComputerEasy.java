0109458ffe6a67de3e3e8d47f1c3fb22515b15d8
nuc006 <nuc006@student.uib.no>
2018-03-23 12:27:37 +0100

    Re-purpose ChessComputerEasy to become ChessComputerMedium. ChessComputerEasy now performs a completely random move

--- Programming/Chess/src/main/java/management/ChessComputerEasy.java
+++ Programming/Chess/src/main/java/management/ChessComputerEasy.java
@@ -16,7 +16,7 @@ public class ChessComputerEasy extends ChessComputer {
 
         HashMap<Vector2, IChessPiece> pieces = board.getUsablePieces(alliance());
 
-        List<Move> attacks = new ArrayList<>();
+        //List<Move> attacks = new ArrayList<>();
         List<Move> moves = new ArrayList<>();
 
         for(Vector2 key : pieces.keySet()) {
@@ -28,17 +28,17 @@ public class ChessComputerEasy extends ChessComputer {
 
                 Move move = new Move(piece.position(), destination);
 
-                if(endPiece != null && !piece.alliance().equals(endPiece))
-                    attacks.add(move);
-                else
+                //if(endPiece != null && !piece.alliance().equals(endPiece))
+                //    attacks.add(move);
+                //else
                     moves.add(move);
             }
         }
 
-        if(attacks.size() != 0)
-            return attacks.get(rand.nextInt(attacks.size()));
-        else if(moves.size() != 0)
-            return moves.get(rand.nextInt(moves.size()));
+        //if(attacks.size() != 0)
+        //    return attacks.get(rand.nextInt(attacks.size()));
+        //else if(moves.size() != 0)
+        //    return moves.get(rand.nextInt(moves.size()));
 
         return null;
     }
--- Programming/Chess/src/main/java/management/ChessComputerMedium.java
+++ Programming/Chess/src/main/java/management/ChessComputerMedium.java
@@ -1,123 +1,45 @@
 package management;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
 import pieces.IChessPiece;
-import resources.*;
+import resources.Move;
+import resources.Vector2;
 
 import java.util.*;
 
-
 public class ChessComputerMedium extends ChessComputer {
-    private final long THINKING_TIME = 3000;
-    private Alliance enemy;
-    private ArrayList<MoveScore> moveChart = new ArrayList<>();
-
     public ChessComputerMedium(Board board) {
         super(board);
-        if (alliance() == Alliance.WHITE) {
-            enemy = Alliance.BLACK;
-        } else {
-            enemy = Alliance.WHITE;
-        }
     }
 
-
-    @Override
     public Move getMove() {
-        Board sim;
-        long startTime = System.currentTimeMillis();
+        Random rand = new Random();
 
-        for(IChessPiece piece: board.getUsablePieces(alliance()).values()) {
-            for(Vector2 move: piece.getPossibleDestinations()) {
-                sim = board.clone();
-                sim.movePiece(piece.position(),move);
-                moveChart.add(new MoveScore(scoreBoard(sim), new Move(piece.position(), move)));
-                System.out.println("mediumAI " + piece.toString() + piece.position().getY());
-               /* if(startTime + THINKING_TIME < System.currentTimeMillis()){
-                    printPossibleMoves();
-                    return Collections.max(moveChart).getMove();
+        HashMap<Vector2, IChessPiece> pieces = board.getUsablePieces(alliance());
 
-                }*/
-               printBoard(sim);
+        List<Move> attacks = new ArrayList<>();
+        List<Move> moves = new ArrayList<>();
+
+        for(Vector2 key : pieces.keySet()) {
+            IChessPiece piece = pieces.get(key);
+            Set<Vector2> destinations = piece.getPossibleDestinations("ChessComputerEasy");
+
+            for(Vector2 destination : destinations) {
+                IChessPiece endPiece = board.getPiece(destination);
+
+                Move move = new Move(piece.position(), destination);
+
+                if(endPiece != null && !piece.alliance().equals(endPiece))
+                    attacks.add(move);
+                else
+                    moves.add(move);
             }
         }
-        printPossibleMoves();
 
-        Collections.sort(moveChart);
+        if(attacks.size() != 0)
+            return attacks.get(rand.nextInt(attacks.size()));
+        else if(moves.size() != 0)
+            return moves.get(rand.nextInt(moves.size()));
 
-        Move m = moveChart.get(moveChart.size() - 1).getMove();
-        System.out.println("mediumAI" + m.start.toString() + m.end.toString());
-        return moveChart.get(moveChart.size() - 1).getMove();
-
-
-    }
-    private void printPossibleMoves() {
-        for(MoveScore s: moveChart) {
-            System.out.println(s.getMove().start.toString() + " " + s.getMove().end.toString());
-        }
-    }private void printBoard(Board board) {
-        for (int y = 0; y < board.getSize(); y++) {
-            for (int x = 0; x < board.getSize(); x++) {
-                if(board.getPiece(new Vector2(x,y)) != null) {
-                    System.out.print(board.getPiece(new Vector2(x, y)).getValue());
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    /**
-     *
-     * @param sim the board that gets scored
-     * @return gives score to current state of board
-     */
-    private int scoreBoard(Board sim) {
-        return  diffPieceValue(sim) + diffPossibleMoves(sim);
-    }
-
-    /**
-     *
-     * @param sim
-     * @return gives the difference between the total value of black and white's pieces
-     */
-    private int diffPieceValue(Board sim) {
-        return pieceValue(alliance(), sim) - pieceValue(enemy, sim);
-    }
-
-    private int pieceValue(Alliance alliance, Board sim) {
-        int sum = 0;
-        for(IChessPiece piece: sim.getUsablePieces(alliance).values()) {
-            sum += piece.getValue();
-        }
-        return sum;
-    }
-
-    /**
-     *
-     * @param sim
-     * @return gives the difference between the possible moves black and white have
-     */
-    private int diffPossibleMoves(Board sim) {
-        return possibleMoves(alliance(), sim) - possibleMoves(enemy, sim);
-    }
-
-    private int possibleMoves(Alliance alliance, Board sim) {
-        int sum = 0;
-        for(IChessPiece piece: sim.getPieces(alliance).values()) {
-            for (Vector2 move: piece.getPossibleDestinations()) {
-                sum++;
-            }
-        }
-        return sum;
-    }
-
-    private IChessPiece getRandomPiece(ArrayList<IChessPiece> pieces) {
-        return pieces.get(fromZeroTo(pieces.size() - 1));
-    }
-
-    private int fromZeroTo(int num) {
-        return (int) (Math.random() * num * 1.0);
+        return null;
     }
 }
