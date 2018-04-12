87aff713560106134347f4e7a1b24f02cb66da6b
jbe091 <jbe091@uib.no>
2018-04-12 15:27:02 +0200

    starting on primer phase of minmax recursion

--- Programming/Chess/src/main/java/management/ChessComputerMedium.java
+++ Programming/Chess/src/main/java/management/ChessComputerMedium.java
@@ -1,13 +1,17 @@
 package management;
 
+import pieces.*;
 import resources.*;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class ChessComputerMedium extends ChessComputer {
 
     private final int DEPTH = 3;
     private Alliance enemy;
+    private final Alliance black = Alliance.BLACK;
+    private final Alliance white = Alliance.WHITE;
     private ArrayList<MoveScore> moveChart = new ArrayList<>();
     private int size;
     private int store;
@@ -24,7 +28,6 @@ public class ChessComputerMedium extends ChessComputer {
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
                score(x,y, chessB, DEPTH, turn, 0);
-                turn *= -1;
             }
         }
         return null;
@@ -52,42 +55,19 @@ public class ChessComputerMedium extends ChessComputer {
 
     private int diagonals(int x, int y, int[][] chessB, int depth, int turn, int score) {
         int sum = score;
-        int[][] clone;
         for (int i = 0; i < size ; i++) {
-            clone = chessB.clone();
-            sum += move(clone,x,y,x,y);
-            sum += calcOtherSide(clone, depth, turn * -1, 0);
-            clone = chessB.clone();
-            sum += move(clone,x,y,x++,y++);
-            sum += calcOtherSide(clone, depth, turn * -1, 0);
-            clone = chessB.clone();
-            sum += move(clone,x,y,x++,y--);
-            sum += calcOtherSide(clone, depth, turn * -1, 0);
-            clone = chessB.clone();
-            sum += move(clone,x,y,x--,y++);
-            sum += calcOtherSide(clone, depth, turn * -1, 0);
-        }
-        return sum;
-    }
-    private int inStraights(int x, int y, int[][] chessB, int depth, int turn, int score) {
-        int sum = score;
-        int[][] clone;
-        for (int i = 0; i < size ; i++) {
-            clone = chessB.clone();
-            sum += move(clone,x,y,x,y--);
-            sum += calcOtherSide(clone), depth, turn * -1, 0);
-            sum += move(clone,x,y,x,y++);
-            sum += calcOtherSide(chessB.clone(), depth, turn * -1, 0);
-            sum += move(clone,x,y,x++,y);
-            sum += calcOtherSide(chessB.clone(), depth, turn * -1, 0);
-            sum += move(clone,x,y,x--,y);
-            sum += calcOtherSide(chessB.clone(), depth, turn * -1, 0);
+            sum += calcOtherSide(x,y,--x, ++y, chessB, depth, turn * -1, 0);
+            sum += calcOtherSide(x,y,--x, --y, chessB, depth, turn * -1, 0);
+            sum += calcOtherSide(x,y,++x, ++y, chessB, depth, turn * -1, 0);
+            sum += calcOtherSide(x,y,++x, --y, chessB, depth, turn * -1, 0);
         }
         return sum;
     }
 
-    private int calcOtherSide(int[][] chessB, int depth, int turn, int score) {
+    private int calcOtherSide(int fromX, int fromY, int toX, int toY, int[][] chessB, int depth, int turn, int score) {
         int sum = 0;
+        int[][] clone = chessB.clone();
+        sum += move(clone,fromX, fromY, toX, toY);
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
                 sum += score(x,y, chessB, depth, turn, score);
@@ -97,21 +77,65 @@ public class ChessComputerMedium extends ChessComputer {
     }
 
     private int move(int[][] chessB, int fromX, int fromY, int toX, int toY) {
+        if(insideBoard(fromX,fromY) && insideBoard(toX,toY)) {
+            return 0;
+        }
         store = chessB[toX][toY];
         chessB[toX][toY] = chessB[fromX][fromY];
         chessB[fromX][fromY] = 0;
         return store;//store is the killed piece's value
     }
 
+    private boolean insideBoard(int fromX, int fromY) {
+        return (0 <= fromX && fromX <= size &&
+        0 <= fromY && fromY <= size);
+    }
+
     private int[][] translateBoard() {
         int[][] chessB = new int[size][size];
+        ChessPiece selectedPiece;
+        Vector2 position;
+        Alliance color;
+
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
-
+                position = new Vector2(x,y);
+                selectedPiece = (ChessPiece) board.getPiece(position);
+                color = selectedPiece.alliance();
+                if(selectedPiece == null) continue;
+                else if (selectedPiece instanceof Pawn && color == black) chessB[x][y] = 1;
+                else if (selectedPiece instanceof Pawn && color == white) chessB[x][y] = -1;
+                else if (selectedPiece instanceof Knight && color == black) chessB[x][y] = 3;
+                else if (selectedPiece instanceof Knight && color == white) chessB[x][y] = -3;
+                else if (selectedPiece instanceof Bishop && color == black) chessB[x][y] = 4;
+                else if (selectedPiece instanceof Bishop && color == white) chessB[x][y] = -4;
+                else if (selectedPiece instanceof Rook && color == black) chessB[x][y] = 5;
+                else if (selectedPiece instanceof Rook && color == white) chessB[x][y] = -5;
+                else if (selectedPiece instanceof King && color == black) chessB[x][y] = 2;
+                else if (selectedPiece instanceof King && color == white) chessB[x][y] = -2;
+                else if (selectedPiece instanceof Queen && color == black) chessB[x][y] = 9;
+                else if (selectedPiece instanceof Queen && color == white) chessB[x][y] = -9;
             }
         }
         return chessB;
     }
-
-
+    public void printChessb(int[][] chessB) {
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                if (chessB[x][y] == 1) System.out.print('p');       //pawn
+                else if (chessB[x][y] == -1) System.out.print('P');
+                else if (chessB[x][y] == 3) System.out.print('h');  //Knight
+                else if (chessB[x][y] == -3) System.out.print('H');
+                else if (chessB[x][y] == 4) System.out.print('b');  //Bishop
+                else if (chessB[x][y] == -4) System.out.print('B');
+                else if (chessB[x][y] == 5) System.out.print('r');  //Rook
+                else if (chessB[x][y] == -5) System.out.print('R');
+                else if (chessB[x][y] == 2) System.out.print('k');  //King
+                else if (chessB[x][y] == -2) System.out.print('K');
+                else if (chessB[x][y] == 9) System.out.print('q');  //Queen
+                else if (chessB[x][y] == -9) System.out.print('Q');
+            }
+            System.out.println();
+        }
+    }
 }
