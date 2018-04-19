15c63eec9bbed0973c3f8e27c49d5f1e8bb2ae0f
jbe091 <jbe091@uib.no>
2018-04-19 23:56:03 +0200

    change difficulty medium

--- Programming/Chess/src/main/java/management/ChessComputerMedium.java
+++ Programming/Chess/src/main/java/management/ChessComputerMedium.java
@@ -2,6 +2,6 @@ package management;
 public class ChessComputerMedium extends ChessComputerHard {
     public ChessComputerMedium(Board board) {
         super(board);
-        thinkTime = 1000;
+        thinkTime = 1;
     }
 }
