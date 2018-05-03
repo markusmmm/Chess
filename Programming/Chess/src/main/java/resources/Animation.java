42b75ea9a35e6091381dbcb2699958efec05eab7
jbe091 <jbe091@uib.no>
2018-05-03 14:05:29 +0200

    added class Animation, for handling animations

---
+++ Programming/Chess/src/main/java/resources/Animation.java
@@ -0,0 +1,10 @@
+package resources;
+
+import main.Tile;
+
+public class Animation {
+    private Tile[][] grid;
+    public Animation(Tile[][] grid, String pictureFolder) {
+        this.grid = grid;
+    }
+}
