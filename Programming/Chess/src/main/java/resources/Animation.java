74c9613705ec74de3476152dfbdef09f7107d591
jbe091 <jbe091@uib.no>
2018-05-03 16:15:22 +0200

    Animation-class implemented, but not tested

--- Programming/Chess/src/main/java/resources/Animation.java
+++ Programming/Chess/src/main/java/resources/Animation.java
@@ -1,18 +1,16 @@
 package resources;
 
-import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 import main.Tile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Animation {
-    private ArrayList<BufferedImage> img = new ArrayList<>();
-    private int timeBetweenFrames;
+    private ArrayList<ImagePattern> img = new ArrayList<>();
+    private long MillSecFrame;
     private Tile[][] grid;
     private static final String[] SUPPORTED_EXTENSIONS = {"png"};
 
@@ -30,27 +28,36 @@ public class Animation {
         }
     };
 
+    /**
+     *
+     * @param grid Tile object where animation take plase
+     * @param picturePath folder with images
+     * @param timeBetweenFrames time between each frame
+     */
     public Animation(Tile[][] grid, String picturePath, int timeBetweenFrames) {
         this.grid = grid;
-        this.timeBetweenFrames = timeBetweenFrames;
+        this.MillSecFrame = timeBetweenFrames;
         loadPictures(picturePath);
     }
     public void play(Vector2 p) {
-        for (BufferedImage img: img) {
-            //grid[p.getY()][p.getX()].setFill();
+        for (ImagePattern img: img) {
+            grid[p.getY()][p.getX()].setFill(img);
+
+            try {
+                wait(MillSecFrame);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private void loadPictures(String picturePath) {
-        File dir = new File("PATH_TO_YOUR_DIRECTORY");
+        File dir = new File(picturePath);
+        ImagePattern img;
         if(dir.isDirectory()) {
             for(File f: dir.listFiles(IMAGE_FILTER)) {
-                try {
-                    img.add(ImageIO.read(f));
-                }
-                catch (final IOException e) {
-                    // handle errors here
-                }
+                img = new ImagePattern(new Image(f.getPath(), 64, 64, true, true));
+                this.img.add(img);
             }
         }
 
