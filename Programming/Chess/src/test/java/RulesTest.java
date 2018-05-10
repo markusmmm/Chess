a5e37b27795655ae2c62641cde077bea10d5a0a6
Mikael Røsbak Hanssen <nuc006@uib.no>
2018-05-10 13:18:32 +0200

    Add test for black en-passant

--- Programming/Chess/src/test/java/RulesTest.java
+++ Programming/Chess/src/test/java/RulesTest.java
@@ -13,7 +13,9 @@ public class RulesTest {
     BoardLibrary boards = new BoardLibrary(Main.TESTS_DIR);
 
     // Naming convention: pieceToTest|cantMove(Expected result: false)|relativeMoveDestination
-    // More general tests break this naming convention (e.g. allyCantSetCheck)
+    // More general tests break this naming convention (e.g. allyCantSetCheckWithMove)
+
+    //Basic movement tests
     @Test public void rookN()                           { perform("rook",                    true,  new Move(0,7,0,4)); }
     @Test public void rookE()                           { perform("rook",                    true,  new Move(0,7,4,7)); }
     @Test public void rookCantMoveNE()                  { perform("rook",                    false, new Move(0,7,1,6)); }
@@ -35,16 +37,20 @@ public class RulesTest {
     @Test public void pawn2N()                          { perform("pawn",                    true,  new Move(7,6,7,5)); }
     @Test public void pawnN()                           { perform("pawn",                    true,  new Move(7,6,7,4)); }
 
-    @Test public void enPassant()                       { perform("enPassant",               true,  new Move(0,3,1,2)); }
-    @Test public void allyCantSetCheck()                { perform("allyCantSetCheck",        false, new Move(3,1,4,2)); }
-    @Test public void kingCanAttackIntruder()           { perform("kingCanAttackIntruder",   true,  new Move(4,0,3,0)); }
-    @Test public void pawnCanResolveCheck()             { perform("pawnCanResolveCheck",     true,  new Move(2,6,3,5)); }
-    @Test public void pawnCantThreatenWithMove()        { perform("pawnCantThreatenWithMove",true,  new Move(3,7,3,6)); }
-    @Test public void attackCanResolveCheck()           { perform("threatenedAttacker",      true,  new Move(2,1,3,2)); }
+    //Special movement tests
+    @Test public void whiteEnPassant()                  { perform("whiteEnPassant",          true,  new Move(0,3,1,2)); }
+    @Test public void blackEnPassant()                  { perform("blackEnPassant",          true,  new Move(0,3,1,2)); }
 
     @Test public void castlingKingSide()                { perform("castling",                true,  new Move(4,7,6,7)); }
     @Test public void castlingQueenSide()               { perform("castling",                true,  new Move(4,7,2,7)); }
     @Test public void cantCastleOverCheckedArea()       { perform("castlingOverCheckedArea", false, new Move(4,7,2,7)); }
+
+    //Behaviour tests
+    @Test public void allyCantSetCheckWithMove()        { perform("allyCantSetCheckWithMove",false, new Move(3,1,4,2)); }
+    @Test public void kingCanAttackIntruder()           { perform("kingCanAttackIntruder",   true,  new Move(4,0,3,0)); }
+    @Test public void pawnCanResolveCheck()             { perform("pawnCanResolveCheck",     true,  new Move(2,6,3,5)); }
+    @Test public void pawnCantThreatenWithMove()        { perform("pawnCantThreatenWithMove",true,  new Move(3,7,3,6)); }
+    @Test public void attackCanResolveCheck()           { perform("threatenedAttacker",      true,  new Move(2,1,3,2)); }
 
     public void perform(String testName, boolean expectedResult, Move... moves) {
         boolean actual = true;
